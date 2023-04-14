b767d1ce6fef4904e64eb9b10effc91ae5983b42
DarrenNA <darrenndouongo@gmail.com>
2023-04-15 00:30:51 +0200

    Install and import tailwindcss aswell as hero icon.install vue.js and created first view Login and Dashboard only containing vue.component.created routes before that in  a router folder directing the routes of the project meaning accessing the views.And Deleted the first template of app.vue and the hello world

--- backend/src/main.js
+++ backend/src/main.js
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
+import routes from './router'
+import store from './store'
 import './index.css'
 import App from './App.vue'
+import router from './router'
 
-createApp(App).mount('#app')
+createApp(App)
+.use(store)
+.use(router)
+.mount('#app')
